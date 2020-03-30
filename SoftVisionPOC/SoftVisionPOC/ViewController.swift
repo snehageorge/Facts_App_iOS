e44b46ae17201967c08edc40e1e0ab6683fd2d9c
Sneha <snehag@qburst.com>
2020-03-30 13:10:49 +0530

    - Project setup.

---
+++ SoftVisionPOC/SoftVisionPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftVisionPOC
+//
+//  Created by Sneha G on 30/03/20.
+//  Copyright © 2020 QBurst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoftVisionPOC/SoftVisionPOCTests/SoftVisionPOCTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoftVisionPOCTests.swift
+//  SoftVisionPOCTests
+//
+//  Created by Sneha G on 30/03/20.
+//  Copyright © 2020 QBurst. All rights reserved.
+//
+
+import XCTest
+@testable import SoftVisionPOC
+
+class SoftVisionPOCTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
