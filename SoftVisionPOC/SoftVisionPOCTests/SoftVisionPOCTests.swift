26b5f549eaefb8cfccc2fb892e6ecc35ffa1f594
Sneha <snehag@qburst.com>
2020-03-31 08:16:37 +0530

    - Added API and UI component test cases

--- SoftVisionPOC/SoftVisionPOCTests/SoftVisionPOCTests.swift
+++ SoftVisionPOC/SoftVisionPOCTests/SoftVisionPOCTests.swift
@@ -10,24 +10,52 @@ import XCTest
 @testable import SoftVisionPOC
 
 class SoftVisionPOCTests: XCTestCase {
+    var countryInfoViewController:FactsViewController?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        countryInfoViewController = FactsViewController()
+        countryInfoViewController?.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        countryInfoViewController = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCountryInfoViewControllerUI() {
+        XCTAssertNotNil(countryInfoViewController?.tableView, "Tableview is missing")
+        let refreshControl = countryInfoViewController?.refreshControl
+        XCTAssertNotNil(refreshControl, "Refresh control is missing")
+        let action = refreshControl?.actions(forTarget: countryInfoViewController, forControlEvent: .valueChanged)
+        XCTAssertNotNil(action,"Pull to refresh is not attached to value changed event.")
+    }
+
+    func testCountryInfoAPIMockResponse() {
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: "factResponse", ofType: "json")
+        let data = try? Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped)
+        do {
+            let decodedData: FactResponseModel = try JSONDecoder().decode(FactResponseModel.self, from: data!)
+            XCTAssertTrue(decodedData.title != nil && decodedData.rows != nil, "Invalid response")
+            XCTAssertTrue(decodedData.rows?.count == 14, "Invalid number of rows")
+            let viewModel = FactViewModel(factList: decodedData)
+            XCTAssertTrue(viewModel.factList?.count == 13, "Invalid number of rows")
+        } catch {
+            XCTFail("Unable to parse the response")
+        }
+    }
+
+    func testCountryInfoAPICall() {
+        let promise = expectation(description: "Status code between 200 and 299")
+        APIManager.factAPI(successBlock: { (respModel) in
+            promise.fulfill()
+        }, failureBlock: { (errMsg) in
+            XCTFail("Error Message: " + errMsg)
+        })
+        wait(for: [promise], timeout: 20)
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 
